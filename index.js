cdbd8d9d14ccd8b92d5942e6b8dbdacc541a0174
ohheyimjen <jendority@gmail.com>
2019-04-29 12:56:49 -0700

    Initialized Project and implemented API

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db.js')
+
+const server = express();
+
+server.use(express.json());
+
+server.listen(4000, () => {
+    console.log('Server running on localhost:4000');
+})
+
+server.get('/', (req, res) => {
+    res.send('Web Api Challenge');
+});
