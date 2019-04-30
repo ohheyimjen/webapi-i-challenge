a02c1a41984d847904886960219464d166fabede
ohheyimjen <jendority@gmail.com>
2019-04-29 17:34:35 -0700

    Completed mvps

--- README.md
+++ README.md
@@ -53,10 +53,10 @@ Inside `index.js` add the code necessary to implement the following _endpoints_:
 
 | Method | URL            | Description                                                                                                                       |
 | ------ | -------------- | --------------------------------------------------------------------------------------------------------------------------------- |
-| POST   | /api/users     | Creates a user using the information sent inside the `request body`.                                                              |
-| GET    | /api/users     | Returns an array of all the user objects contained in the database.                                                               |
-| GET    | /api/users/:id | Returns the user object with the specified `id`.                                                                                  |
-| DELETE | /api/users/:id | Removes the user with the specified `id` and returns the deleted user.                                                            |
+[X]| POST   | /api/users     | Creates a user using the information sent inside the `request body`.                                                              |
+[X]| GET    | /api/users     | Returns an array of all the user objects contained in the database.                                                               |
+[X]| GET    | /api/users/:id | Returns the user object with the specified `id`.                                                                                  |
+[X]| DELETE | /api/users/:id | Removes the user with the specified `id` and returns the deleted user.                                                            |
 | PUT    | /api/users/:id | Updates the user with the specified `id` using data from the `request body`. Returns the modified document, **NOT the original**. |
 
 #### Endpoint Specifications
--- index.js
+++ index.js
@@ -1,15 +1,88 @@
 // implement your API here
-const express = require('express');
-const db = require('./data/db.js')
+const express = require("express");
+const db = require("./data/db");
 
 const server = express();
 
 server.use(express.json());
 
 server.listen(4000, () => {
-    console.log('Server running on localhost:4000');
-})
+  console.log("Server running on localhost:4000");
+});
 
-server.get('/', (req, res) => {
-    res.send('Web Api Challenge');
+server.get("/", (req, res) => {
+  res.send("Web Api Challenge");
+});
+
+server.post("/api/users", (req, res) => {
+  const userInfo = req.body;
+  db.insert(userInfo)
+    .then(user => {
+      if (!user.name || !user.bio) {
+        res.status(400).json({
+          success: false,
+          errorMessage: "No name, no bio, no service."
+        });
+      } else {
+        res.status(201).json({
+          success: true,
+          user
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        success: false,
+        error: "There was an error while saving the user to the database"
+      });
+    });
+});
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json({ users });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "error retrieving users" });
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+});
+
+server.delete("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.remove(id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch(err => {
+      res.status(500).json({ success: true, message: err.message });
+    });
+});
+
+server.put("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  db.update(id, changes)
+    .then(updated => {
+      if (updated) {
+        res.status(200).json({ success: true, updated });
+      } else {
+        res.status(404).json({
+          success: false,
+          message: "I cannot find the user you are looking for 💩"
+        });
+      }
+    })
+    .catch(({ code, message }) => {
+      res.status(code).json({
+        success: false,
+        message
+      });
+    });
 });
